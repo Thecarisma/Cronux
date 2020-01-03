fa29ac17ecea89c761cb25740dac2f5acd710418
Azeez Adewale <azeezadewale98@gmail.com>
2020-01-03 12:33:25 +0100

    create the ad function and execute command function

--- windows/Cronux.ps1
+++ windows/Cronux.ps1
@@ -1,1 +1,38 @@
-$args[0]
+
+$annoyance = "if you've benefited from this project, consider supporting `nme on Patreon https://patreon.com/thecarisma this is it we are here am the light of the world`nand I know it"
+
+Function main {
+    Print-Advert $annoyance
+    Execute-Command $args
+    Print-Advert $annoyance
+}
+
+Function Execute-Command {    
+    Param($params)
+    
+    $params[0]
+}
+
+Function Print-Advert {
+    Param([string]$ad)
+    
+    $lines = $ad.Split("`n")
+    $length = 0
+    ForEach ($line in $lines) {
+        If ($line.length -gt $length - 2) {
+            $length = $line.length + 2
+        }
+    }
+    $i = $length
+    While (--$i -gt 0) {
+        Write-Host -NoNewline  "=" 
+    }
+    ""
+    $ad
+    While (++$i -lt $length) {
+        Write-Host -NoNewline  "=" 
+    }
+    ""
+}
+
+main $args
