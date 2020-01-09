281766484923ab4ce8c8f2e4b7e316840d444775
Azeez Adewale <azeezadewale98@gmail.com>
2020-01-09 17:27:26 +0100

    complete command to install pfx command

---
+++ commands/installpfx.ps1
@@ -0,0 +1,58 @@
+<#
+.SYNOPSIS
+    Generate a code signing certificate.
+.DESCRIPTION
+    Generate a code signing certificate to sign your application, 
+    powershell script e.t.c. The certificate is generated into the 
+    'cert:\LocalMachine\My' certificate store location and moved to 
+    and moved to 'Cert:\LocalMachine\Root' to mark it as a trusted 
+    certificate. Always put the first argument which is the name of 
+    the certificate in a single quote e.g. `'Cronux Certificate'`.
+    
+    After generating the certificate, it is exported into the output 
+    folder specified in the third parameter. 
+    
+    If a certificate already exist with the same Subject name the old 
+    certificate is removed and replaced with the new certificate that 
+    is generated.
+.INPUTS 
+    System.String[]
+.OUTPUTS 
+    System.String
+.NOTES
+    Version    : 1.0
+    File Name  : installpfx.ps1
+    Author     : Adewale Azeez - azeezadewale98@gmail.com
+    Date       : Jan-09-2019
+.LINK
+    https://thecarisma.github.io/Cronux
+.EXAMPLE
+    installpfx 'Cronux' 3 ./dist/ mypasssword
+    This generates the Cronux certificate in 'cert:\LocalMachine\My' 
+    and exports Cronux.pfx to ./dist/ folder. The generated certificate 
+    expires after 3 years.
+.EXAMPLE
+    installpfx 'My Code Signer' 1 ./dist/ mypasssword
+    This generates the Cronux certificate in 'cert:\LocalMachine\My' 
+    and exports Cronux.pfx to ./dist/ folder. The generated certificate 
+    expires after 1 years.
+    
+#>
+
+Param(
+    [Parameter(mandatory=$true)]
+    [string]$cert_path,
+    [Parameter(mandatory=$true)]
+    [string]$passsword,
+    [Parameter(mandatory=$true)]
+    [string]$store_location
+)
+
+$cert_path = [System.IO.Path]::GetFullPath($cert_path)
+
+If ( -not [System.IO.File]::Exists($cert_path)) {
+    Write-Error "$cert_path does not exist"
+    Return
+}
+
+Import-Certificate -CertStoreLocation $store_location -FilePath $cert_path
