377d79ccbe72ca47a279ad43b23f713f99927e6d
Azeez Adewale <azeezadewale98@gmail.com>
2020-01-09 17:12:10 +0100

    complete command to create password certificate

--- commands/gencodesigncert.ps1
+++ commands/gencodesigncert.ps1
@@ -6,13 +6,15 @@
     powershell script e.t.c. The certificate is generated into the 
     'cert:\LocalMachine\My' certificate store location and moved to 
     and moved to 'Cert:\LocalMachine\Root' to mark it as a trusted 
-    certificate. 
+    certificate. Always put the first argument which is the name of 
+    the certificate in a single quote e.g. `'Cronux Certificate'`.
     
-    Aftet generating the certificate, it is exported into the output 
+    After generating the certificate, it is exported into the output 
     folder specified in the third parameter. 
     
-    Always put the first argument which is the name of the certificate 
-    in a single quote e.g. `'Cronux Certificate'`.
+    If a certificate already exist with the same Subject name the old 
+    certificate is removed and replaced with the new certificate that 
+    is generated.
 .INPUTS 
     System.String[]
 .OUTPUTS 
@@ -25,12 +27,12 @@
 .LINK
     https://thecarisma.github.io/Cronux
 .EXAMPLE
-    gencodesigncert 'Cronux' 3 ./dist/
+    gencodesigncert 'Cronux' 3 ./dist/ mypasssword
     This generates the Cronux certificate in 'cert:\LocalMachine\My' 
     and exports Cronux.pfx to ./dist/ folder. The generated certificate 
     expires after 3 years.
 .EXAMPLE
-    gencodesigncert 'My Code Signer' 1 ./dist/
+    gencodesigncert 'My Code Signer' 1 ./dist/ mypasssword
     This generates the Cronux certificate in 'cert:\LocalMachine\My' 
     and exports Cronux.pfx to ./dist/ folder. The generated certificate 
     expires after 1 years.
@@ -41,13 +43,53 @@ Param(
     [Parameter(mandatory=$true)]
     [string]$cert_name,
     [Parameter(mandatory=$true)]
-    [int]$expiry_year,
+    [int]$expiry_year_,
     [Parameter(mandatory=$true)]
-    [string]$output_folder_path
+    [string]$output_folder_path,
+    [Parameter(mandatory=$true)]
+    [string]$password_
 )
 
-$cert_name
-$expiry_year
-$output_folder_path
+$password = ConvertTo-SecureString -String "$($password_)" -Force -AsPlainText
+$store_location = "Cert:\LocalMachine"
+$expiry_year = (Get-Date).AddYears($expiry_year_)
+$output_folder_path = [System.IO.Path]::GetFullPath($output_folder_path)
 
-#$cert = New-SelfSignedCertificate -Subject "$cert_name" -Type CodeSigningCert -CertStoreLocation cert:\LocalMachine\My
+If ( -not [System.IO.Directory]::Exists($output_folder_path)) {
+    [System.IO.Directory]::CreateDirectory($output_folder_path) > $null
+    If ( -not $?) {
+        Return
+    }
+}
+
+Function Find-Delete-Certificate {
+    Param([string]$cert_subject_name)
+
+    ForEach ($cert in (ls "$store_location\My")) {
+        If ($cert.Subject -eq "CN=$cert_subject_name") {
+            "Removing already existing certificate $store_location\My"
+            $cert | Remove-Item
+            If ( -not $?) {
+                Return
+            }
+        }
+    }
+    ForEach ($cert in (ls "$store_location\Root")) {
+        If ($cert.Subject -eq "CN=$cert_subject_name") {
+            "Removing already existing certificate in $store_location\Root"
+            $cert | Remove-Item
+            If ( -not $?) {
+                Return
+            }
+        }
+    }
+}
+
+Find-Delete-Certificate $cert_name
+"Generating and installing certificate"
+$cert = New-SelfSignedCertificate -Subject "$cert_name" -notafter $expiry_year -Type CodeSigningCert -CertStoreLocation "$store_location\My"
+"Exporting the certificate to $output_folder_path\$cert_name.pfx"
+Export-PfxCertificate -Cert "$store_location\My\$($cert.Thumbprint)" -FilePath "$output_folder_path\$cert_name.pfx" -Password $password
+Find-Delete-Certificate $cert_name
+# "Adding certificate to trusted path..."
+# Move-Item -Path $cert.PSPath -Destination "$store_location\Root"
