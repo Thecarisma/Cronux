8f161c31d1f7ab9650119d67775c1e40b93550d6
Azeez Adewale <azeezadewale98@gmail.com>
2020-02-17 16:20:04 +0100

    add base64 encoder script

--- commands/base64.ps1
+++ commands/base64.ps1
@@ -1,3 +1,39 @@
+<#
+.SYNOPSIS
+    Convert file to base64
+.DESCRIPTION
+    Convert a file to it base64 value, write the base64 
+    result into a file, copy it to clipboard or generates 
+    a valid JSON Object. 
+.INPUTS 
+    The path to file to get it base64 value
+.OUTPUTS 
+    Depends on switches
+.NOTES
+    Version    : 1.0
+    File Name  : base64.ps1
+    Author     : Philip Mateescu - https://www.reddit.com/user/philipmat/
+    Date       : 2019
+.LINK
+    https://thecarisma.github.io/Cronux
+    https://philipm.at/2018/base64_in_powershell.html
+    https://www.reddit.com/r/PowerShell/comments/800jnc/converttobase64_encodes_files_to_base64/
+.EXAMPLE
+    base64 C:/path/to/file.extension -WriteToFile
+    The command encodes the file in base64 and writes it 
+    to file in the same directory as the file with the 
+    extension '.base64' 
+.EXAMPLE
+    base64 C:/path/to/file.extension -AndCopyToClipboard
+    The command encodes the file in base64 and copies the 
+    base64 value to clipboard  
+.EXAMPLE
+    base64 C:/path/to/file.extension -ToJson
+    The command encodes the file in base64 and generates a 
+    valid JSON object of the base64 value
+    
+#>
+
 [CmdletBinding()]
 Param(
 	[Parameter(Mandatory=$True, Position=0)]
@@ -17,7 +53,6 @@ if ((Test-Path $InputFile) -eq $false) {
 $base64String = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($InputFile))
 if ($ToJson) {
 	Add-Type -AssemblyName System.Web.Extensions
-	# [System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
 	$jsonSerializer = New-Object System.Web.Script.Serialization.JavaScriptSerializer 
 	$base64String = $jsonSerializer.Serialize(@{ content = $base64String })
 }
