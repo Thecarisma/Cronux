83a997e4031f75391c230c8030ba337c20a60e36
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-15 14:02:59 +0100

    properly expost most area of psdoc to markdown

--- commands/documentation/markdownpsdoc.ps1
+++ commands/documentation/markdownpsdoc.ps1
@@ -2,6 +2,11 @@
 .SYNOPSIS
    
 .DESCRIPTION
+    Even though this command do a fine job in converting 
+    a .psdoc file to markdown it not a proper parser, there 
+    are million ways to do this better, but I go for the 
+    fastest and easiest method in this script.
+    
     Add the -Verbose switch to see more output in the 
     shell
 .INPUTS 
@@ -32,7 +37,9 @@ Param(
     # whether to generate table of content
     [switch]$Toc,
     # do not use html to position and format document
-    [switch]$SkipHtml
+    [switch]$SkipHtml,
+    # convert psdoc to markdown for markdown2rst
+    [switch]$ForRST
 )
 
 $PsDocPath = [System.IO.Path]::GetFullPath($PsDocPath)
@@ -54,7 +61,7 @@ Function main {
         If ( -not [System.IO.File]::Exists($PsDocPath)) {
             Return
         }
-        PsDoc-to-Markdown $PsDocPath
+        PsDoc-to-Markdown $PsDocPath $OutputFolder
     }
 }
 
@@ -76,6 +83,14 @@ $Global:res = $false
 $Global:name = ""
 $Global:synopsis = ""
 $Global:description = ""
+$Global:current = ""
+$Global:syntax = ""
+$Global:parsing_paramtype = 0
+$Global:has_commonparams = $false
+$Global:parameters = ""
+$Global:any = ""
+$Global:long_name = ""
+$Global:notes = ""
 
 Function To-Sentence-Case {
     param(
@@ -92,6 +107,7 @@ Function Parse-Name {
         return
     }
     
+    $Global:long_name = $argument
     $Splited = $argument.Split("\")
     $Global:name = $Splited[$Splited.Length - 1].Split(".ps1")[0]
 }
@@ -99,27 +115,71 @@ Function Parse-Name {
 Function Parse-Synopsis {
     param([string]$argument)
     
-    if ($argument.Length -gt 5 -and $argument.StartsWith("   ")) {
-        $argument = $argument.SubString(4, $argument.Length - 5)
+    if ($argument.Length -gt 4 -and $argument.StartsWith("   ")) {
+        $argument = $argument.SubString(4, $argument.Length - 4)
     }
     
     $Global:synopsis += $argument.Replace("    ", " ")
 }
 
-Function Parse-Description {
+Function Parse-Parameters {
     param([string]$argument)
     
-    if ($argument.Length -gt 5 -and $argument.StartsWith("   ")) {
-        $argument = $argument.SubString(4, $argument.Length - 5)
+    if ($argument.Length -gt 4 -and $argument.StartsWith("   ")) {
+        $argument = $argument.SubString(4, $argument.Length - 4)
+         if ($argument.Length -gt 4 -and $argument.StartsWith("   ")) {
+            $argument = $argument.SubString(4, $argument.Length - 4)
+            if ($argument.StartsWith("Required?")) {
+                $Global:parameters += "```````powershell`r`n"
+            }
+            Parse-Any $argument
+            $Global:parameters += $Global:current
+            $Global:current = ""
+        } else {
+            if (-not [string]::IsNullOrWhitespace($Global:parameters) -and $argument.StartsWith("-")) {
+                $Global:parameters += "```````r`n`r`n"
+            }
+            if ($argument.StartsWith("<") -and $argument.EndsWith(">")) {
+                $argument = $argument.SubString(1, $argument.Length - 2);
+                $Global:parameters += "```````r`n`r`n"
+                $Global:parameters += "### $argument`r`n`r`n"
+                $Global:has_commonparams = $true
+                return
+            }
+            if (-not [string]::IsNullOrWhitespace($argument)) {
+                $Global:parameters += "### $argument`r`n"
+            }
+            $Global:parameters += "`r`n"
+        }
     }
-    $Global:description += $argument + "`r`n"
+    
+    #$Global:parameters += $argument
+}
+
+
+Function Parse-Any {
+    param([string]$argument)
+    
+    if ($Global:long_name) {
+        $argument = $argument.Replace($Global:long_name, $Global:name)
+    }
+    if ($argument.Length -gt 4 -and $argument.StartsWith("   ")) {
+        $argument = $argument.SubString(4, $argument.Length - 4)
+    }
+    if ($argument[0] -eq '-' -and -not [string]::IsNullOrWhitespace("$($argument[1])") -or 
+        ($argument.StartsWith("http"))) {
+        $argument = " - " + $argument
+    }
+    $Global:current += $argument + "`r`n"
 }
 
 Function PsDoc-to-Markdown {
     Param(
-        [string]$SinglePsDocPath
+        [string]$SinglePsDocPath,
+        [string]$SavePath
     )
     
+    $name_only = [System.IO.Path]::GetFileNameWithoutExtension($SinglePsDocPath)
     [AreaType]$area_type = "Unknown"
     
     ForEach($line in Get-Content $SinglePsDocPath) {
@@ -137,11 +197,24 @@ Function PsDoc-to-Markdown {
                 $area_type = "Description"
                 continue
                 
+            } elseif ($header -eq "PARAMETERS") {
+                $area_type = "Parameters"
+                continue
+                
+            } elseif ($header -eq "SYNTAX") {
+                $area_type = "Syntax"
+                continue
+                
+            } elseif ($header -eq "NOTES") {
+                $area_type = "Notes"
+                continue
+                
             } else {
                 $area_type = "Unknown"
             }
             # set the TOC here and header
-            $ContentMarkdown += "## " + (To-Sentence-Case $line) + "`r`n`r`n"
+            $Global:body += "## " + (To-Sentence-Case $line) + "`r`n`r`n"
+            continue
         }
         
         switch ($area_type) {
@@ -152,12 +225,25 @@ Function PsDoc-to-Markdown {
                 Parse-Synopsis $line 
             }
             Description {
-                Parse-Description $line 
+                Parse-Any $line 
+                $Global:description += $Global:current
+            }
+            Parameters {
+                Parse-Parameters $line 
+            }
+            Notes {
+                Parse-Notes $line 
+            }
+            Syntax {
+                Parse-Any $line 
+                $Global:syntax += $Global:current
             }
             Unknown {
-                
+                Parse-Any $line 
+                $Global:body += $Global:current
             }
         }
+        $Global:current = ""
     }
     
     $title_part = $Global:name
@@ -169,6 +255,9 @@ Function PsDoc-to-Markdown {
     if (-not $SkipHtml) {
         $brief_part = "<p style=`"text-align: center;`" align=`"center`">{0}</p>" -f $Global:synopsis
     }
+    if (-not $Global:has_commonparams) {
+        $Global:parameters += "```````r`n`r`n"
+    }
     
     $ContentMarkdown = "
 # {0}
@@ -177,13 +266,29 @@ Function PsDoc-to-Markdown {
 
 ---
 
-{2}    
+{2}  
+
+## Syntax
+
+``````powershell
+{3}  
+``````
+
+## Parameters
+
+{4}
+
+{5}
     " -f 
       $title_part,
       $brief_part,
-      $Global:description
+      $Global:description,
+      $Global:syntax.Trim(),
+      $Global:parameters.Trim(),
+      $Global:body
         
-    $ContentMarkdown
+    $Global:notes
+    [System.IO.File]::WriteAllLines("$SavePath\$name_only.md",  $ContentMarkdown)
 }
 
 main
