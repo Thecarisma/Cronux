41b6002fed2e5d2833491cab8940640d32dea524
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-15 16:42:08 +0100

    create script to generate markdown from .ps1 file: test travis test 2

--- commands/Cronux.ps1
+++ commands/Cronux.ps1
@@ -66,6 +66,7 @@ Function Iterate-Folder {
                         $escaped_params.Add("'$($param)'")
                     }
                 }
+                "Found: $_.FullName"
                 powershell $_.FullName  @($escaped_params)
                 $Global:found_command = $true
                 Return
