a3b788fe876415c6cf29d29680db00899ded57f1
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-15 18:51:01 +0100

    create script to generate markdown from .ps1 file: test travis test 13

--- commands/documentation/ps12markdown.ps1
+++ commands/documentation/ps12markdown.ps1
@@ -21,4 +21,16 @@
     
 #>
 
-"Hello World"
+
+"Hello World 1"
+& "$PSScriptRoot\psdoc.ps1" $Path "$OutputFolder" -Recurse:$Recurse -Silent:$Silent 
+"Hello World 2"
+& "$PSScriptRoot\psdoc2markdown.ps1" $OutputFolder $OutputFolder -SkipHtml:$SkipHtml -SkipNotes:$SkipNotes -Silent:$Silent -Recurse:$Recurse
+"Hello World 3"
+
+If (-not $Keep) {
+    if (-not $Silent) {
+        "Cleaning up generated *.psdoc files"
+    }
+    Get-ChildItem $OutputFolder -Recurse | Where{$_.Name -Match "(.*?)(\.psdoc)"} | Remove-Item
+}
