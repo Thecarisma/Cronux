fc570adc4bb5f480e26551e74c2d8bdf9419f8b2
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-15 18:49:26 +0100

    create script to generate markdown from .ps1 file: test travis test 13

--- commands/documentation/ps12markdown.ps1
+++ commands/documentation/ps12markdown.ps1
@@ -21,34 +21,4 @@
     
 #>
 
-Param(
-    # The path to the powershell scri[t to extract documentation
-    [Parameter(Mandatory=$true, Position=0)]
-    [string]$Path,
-    # the folder to put generated exported documenation
-    [Parameter(Mandatory=$true, Position=1)]
-    [string]$OutputFolder,
-    # do not use html to position and format document
-    [switch]$SkipHtml,
-    # do not add notes detail before description
-    [switch]$SkipNotes,
-    # whether to print anythin to the console
-    [switch]$Silent,
-    # generate markdown in subfolders?
-    [switch]$Recurse,
-    # do not delete the generated .psdoc files in OutputFolder
-    [switch]$Keep
-)
-
-"Hello World 1"
-& "$PSScriptRoot\psdoc.ps1" $Path "$OutputFolder" -Recurse:$Recurse -Silent:$Silent 
-"Hello World 2"
-& "$PSScriptRoot\psdoc2markdown.ps1" $OutputFolder $OutputFolder -SkipHtml:$SkipHtml -SkipNotes:$SkipNotes -Silent:$Silent -Recurse:$Recurse
-"Hello World 3"
-
-If (-not $Keep) {
-    if (-not $Silent) {
-        "Cleaning up generated *.psdoc files"
-    }
-    Get-ChildItem $OutputFolder -Recurse | Where{$_.Name -Match "(.*?)(\.psdoc)"} | Remove-Item
-}
+"Hello World"
