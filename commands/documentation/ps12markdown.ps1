7fa064281658da44834f8af65f4599ab438ede0a
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-16 05:34:30 +0100

    create script to generate markdown from .ps1 file: test travis test 17

--- commands/documentation/ps12markdown.ps1
+++ commands/documentation/ps12markdown.ps1
@@ -21,6 +21,24 @@
     
 #>
 
+Param(
+    # The path to the powershell scri[t to extract documentation
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$Path,
+    # the folder to put generated exported documenation
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$OutputFolder,
+    # do not use html to position and format document
+    [switch]$SkipHtml,
+    # do not add notes detail before description
+    [switch]$SkipNotes,
+    # whether to print anythin to the console
+    [switch]$Silent,
+    # generate markdown in subfolders?
+    [switch]$Recurse,
+    # do not delete the generated .psdoc files in OutputFolder
+    [switch]$Keep
+)
 
 & "$PSScriptRoot\psdoc.ps1" $Path "$OutputFolder" -Recurse:$Recurse -Silent:$Silent 
 & "$PSScriptRoot\psdoc2markdown.ps1" $OutputFolder $OutputFolder -SkipHtml:$SkipHtml -SkipNotes:$SkipNotes -Silent:$Silent -Recurse:$Recurse
