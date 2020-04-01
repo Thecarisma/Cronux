0b55c0608de539f61d3c1ca4d776de3405a2e05e
Azeez Adewale <azeezadewale98@gmail.com>
2020-04-01 15:09:02 +0100

    add the elevate command to run app as admin

---
+++ commands/system/elevate.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    Start an application as Administrator
+.DESCRIPTION
+    Start an application as Administrator, the first 
+    argument is the name of the app to run. The following 
+    arguments are sent to the application. 
+.INPUTS 
+    System.String
+.OUTPUTS 
+    [System.String]
+.NOTES
+    Version    : 1.0
+    File Name  : elevate.ps1
+    Author     : Adewale Azeez - azeezadewale98@gmail.com
+    Date       : April-01-2020
+.LINK
+    https://thecarisma.github.io/Cronux
+.EXAMPLE
+    elevate notepad
+    Start the notepad app as Administrator
+.EXAMPLE
+    elevate cmd /k dir
+    Start Windows command prompt as Administrator 
+    and run the command 'dir'. 
+#>
+
+[CmdletBinding()]
+Param(
+    # The application to run as Administrator
+    [string]$App,
+    # The arguments to pass to the application
+    [Parameter(Mandatory=$false, ValueFromRemainingArguments = $true)]
+    [string[]]$ExtraArguments
+)
+
+Start-Process $App \"$ExtraArguments" -Verb RunAs
