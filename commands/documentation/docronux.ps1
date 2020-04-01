314323a7b7ebab948d9bb22c98e2747a95afce02
Azeez Adewale <azeezadewale98@gmail.com>
2020-04-01 16:57:38 +0100

    implements the removecommand command

--- commands/documentation/docronux.ps1
+++ commands/documentation/docronux.ps1
@@ -33,4 +33,4 @@ If ( -not [System.IO.File]::Exists("$dir\Cronux.bat")) {
 }
 
 & "$PSScriptRoot\ps12markdown.ps1" "$dir" "$dir\dist\Cronux.wiki\" -Recurse
-& "$PSScriptRoot\ps12rst.ps1" "$dir" "$dir\dist\gh-pages\" -Recurse
+#& "$PSScriptRoot\ps12rst.ps1" "$dir" "$dir\dist\gh-pages\" -Recurse
