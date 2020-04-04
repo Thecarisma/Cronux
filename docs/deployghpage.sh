264faee537745de6843a277fc7022a34225c7f4f
Adewale Azeez <azeezadewale98@gmail.com>
2020-04-04 21:39:02 +0100

    test travis building gh-pages creates deployghpage.sh

--- docs/deployghpage.sh
+++ docs/deployghpage.sh
@@ -2,6 +2,8 @@ cd dist/
 git clone -b gh-pages https://github.com/Thecarisma/Cronux.git
 cd Cronux/
 cp -r ../gh-pages/build/html/* ./
+git config --local user.email "azeezadewale98@gmail.com"
+git config --local user.name "travis-ci.org"
 git add .; git commit -m "Travis build=${TRAVIS_BUILD_NUMBER}. Update Documentation from Travis CI"
 git push https://Thecarisma:${GITHUB_TOKEN}@github.com/Thecarisma/Cronux.git HEAD:gh-pages;
 cd ../../
