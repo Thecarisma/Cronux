af3df22cf795c9309bb56634883328eac06662d5
Adewale Azeez <azeezadewale98@gmail.com>
2020-04-04 22:02:43 +0100

    test travis building gh-pages creates deployghpage.sh .nojekyll

--- docs/deployghpage.sh
+++ docs/deployghpage.sh
@@ -1,3 +1,16 @@
+
+# Github Wiki
+
+cd dist/
+git clone https://github.com/Thecarisma/Cronux.wiki.git
+cd Cronux.wiki/
+cp -r ../wiki/* ./
+git add .; git commit -m "Travis build=${TRAVIS_BUILD_NUMBER}. Update Wiki from Travis CI"
+git push https://Thecarisma:${GITHUB_TOKEN}@github.com/Thecarisma/Cronux.wiki.git HEAD:master;
+cd ../../
+
+# Upload Github Pages 
+     
 cd dist/
 git clone -b gh-pages https://github.com/Thecarisma/Cronux.git
 cd Cronux/
