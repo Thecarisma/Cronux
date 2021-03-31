c888c7ccc35ce24c1a007ac6098fbcd214c44b96
Adewale Azeez <azeezadewale98@gmail.com>
2021-03-31 13:26:42 +0100

    update the install script to download from main branch

--- docs/deployghpage.sh
+++ docs/deployghpage.sh
@@ -17,6 +17,7 @@ cd dist/
 git clone -b gh-pages https://github.com/Thecarisma/Cronux.git
 cd Cronux/
 cp -r ../../docs/.nojekyll ./
+cp -r ../../commands/cronux/installx.ps1 ./
 cp -r ../gh-pages/build/html/* ./
 git config --local user.email "azeezadewale98@gmail.com"
 git config --local user.name "travis-ci.org"
