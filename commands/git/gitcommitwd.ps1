903d820cfeac125960a5626b9798111cfc464e6d
Adewale Azeez <azeezadewale98@gmail.com>
2021-03-31 13:13:09 +0100

    update the gitcommitwd documentation

--- commands/git/gitcommitwd.ps1
+++ commands/git/gitcommitwd.ps1
@@ -1,12 +1,16 @@
 <#
 .SYNOPSIS
+	Commit or ammend a commit with a custom date.
+.DESCRIPTION
+    Commit with a custom date. To commit in the past set the 
+	day value to - number of days in past. E.g. to commit 
+	3 days ago set the Day value to -3.
     Add and commit all the edited file to git with the 
     commit message as the variadic arguments without quotes 
-.DESCRIPTION
-    A short compact command to quickly add all edited files 
-    to a commit with the commit messages.
     
 .INPUTS 
+	[int]
+	[switch]
     [System.String[]]
 .OUTPUTS 
     [System.String[]]
@@ -19,11 +23,18 @@
     https://git-scm.com/docs/git-add
     https://git-scm.com/docs/git-commit
 .EXAMPLE
-    gitcommit from here is the commit message
-    your changes will be commit with the message 
-    "from here is the commit message". The command is 
+    gitcommit 0 -Amend from here is the commit message
+    your last commit will be ammended with the message 
+    "from here is the commit message" The command is 
     equivalent to 
-    git add .; git commit -m "from here is the commit message";
+    git add .; git commit --amend -m "from here is the commit message";
+.EXAMPLE
+    gitcommit -3 from here is the commit message
+    your changes will be commit with the message 
+    "from here is the commit message" plus the 
+	author and commit date will be set to 3 days ago.
+	The command is equivalent to 
+    git add .; GIT_AUTHOR_DATE='Sun Feb 21 10:03:02 2021 -0400' GIT_COMMITTER_DATE='Sun Feb 21 10:03:02 2021 -0400' git commit -m "from here is the commit message";
 #>
 
 Param(
