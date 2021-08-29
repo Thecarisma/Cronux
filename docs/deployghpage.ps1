9fba18e96a157a63ea3d61b77ceddbe1b211e4e6
Thecarisma <azeezadewale98@gmail.com>
2021-08-29 15:13:51 +0100

    resolve env in powershell deploy script -f

--- docs/deployghpage.ps1
+++ docs/deployghpage.ps1
@@ -8,7 +8,7 @@ cp -r ../wiki/* ./ -Force
 git config --local user.email "azeezadewale98@gmail.com"
 git config --local user.name "thecarisma-ci"
 git add .; git commit -m "Documentation build. Update Wiki from Travis CI"
-git push "https://Thecarisma:$($env:GITHUB_TOKEN)@github.com/Thecarisma/Cronux.wiki.git" HEAD:main
+git push -f "https://Thecarisma:$($env:GITHUB_TOKEN)@github.com/Thecarisma/Cronux.wiki.git" HEAD:main
 cd ../../
 
 # Upload Github Pages 
